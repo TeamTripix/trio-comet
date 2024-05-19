3d98b7bf93cfc91f30b8c2bdc334595db0889a04
TeamTripix <hthakur190@gmail.com>
2024-05-19 12:17:13 +0530

    add meta tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,6 +30,7 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import RightIcon from "../../icons/rightIcon";
 import LeftIcon from "../../icons/leftIcon";
+import { NextSeo } from "next-seo";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -251,6 +252,40 @@ export default function Home() {
 
   return (
     <>
+     <NextSeo
+        title={"this is title"}
+        description="This example uses more of the available config options."
+        canonical="https://www.triocomet.com"
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: "Open Graph Title",
+          description: "Open Graph Description",
+          images: [
+            {
+              url: "https://www.example.ie/og-image-01.jpg",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+            {
+              url: "https://www.example.ie/og-image-02.jpg",
+              width: 900,
+              height: 800,
+              alt: "Og Image Alt Second",
+              type: "image/jpeg",
+            },
+            { url: "https://www.example.ie/og-image-03.jpg" },
+            { url: "https://www.example.ie/og-image-04.jpg" },
+          ],
+          siteName: "SiteName",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
       <Box>
         {bannerApiLoading ? (
           <Skeleton
