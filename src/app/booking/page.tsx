8165b56e83c08a18e0fc60a914d38d6391a2b81d
TeamTripix <hthakur190@gmail.com>
2024-06-16 20:33:04 +0530

    change data format in booking API

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -26,15 +26,14 @@ const Page = () => {
   const token =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjQ4MTAyMjUsInNvdXJjZSI6InNyLWF1dGgtaW50IiwiZXhwIjoxNzE5NDEzMDQ2LCJqdGkiOiJJN2RtNHBRSU5KTVBjOGdVIiwiaWF0IjoxNzE4NTQ5MDQ2LCJpc3MiOiJodHRwczovL3NyLWF1dGguc2hpcHJvY2tldC5pbi9hdXRob3JpemUvdXNlciIsIm5iZiI6MTcxODU0OTA0NiwiY2lkIjo0NTc5NTA4LCJ0YyI6MzYwLCJ2ZXJib3NlIjpmYWxzZSwidmVuZG9yX2lkIjowLCJ2ZW5kb3JfY29kZSI6IiJ9.lkygB8ybk5BO6ghMsjR8BCcT53YDhcwpRX3p2GuUIpc";
   if (merchantID) {
-    axios
-      .post(
+    axios.post(
         "https://apiv2.shiprocket.in/v1/external/orders/create/adhoc",
         {
           headers: {
             Authorization: "Bearer " + token,
           },
         },
-        { ...data }
+        data
       )
       .then((res) => {
         console.log("🚀 ~ .then ~ res:", res);
