362a1d984cede809025e8be4fc56a6513d1fa353
Himanshu <himanshuflexsin@gmail.com>
2024-09-04 12:18:02 +0530

    show products in upper links

---
+++ src/app/combos/Mainpage.tsx
@@ -0,0 +1,198 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Box, Typography, Grid } from "@mui/material";
+import PageSpacing from "@components/PageSpacing";
+import { lightColor, darkColor } from "@/utils/CustomTheme/color";
+import Card from "@components/Card";
+import axios from "axios";
+import BreadCrumb from "@components/BreadCrumb";
+import NoProduct from "../../../icons/noProduct";
+import { useMobile, useTablet } from "@/utils/responsive";
+import ProductCardSkeleton from "@components/Skeleton/ProductCardSkeleton";
+import { useSearchParams } from "next/navigation";
+import { useSelector } from "react-redux";
+
+const ProductCollection = (props: any) => {
+  const { params, product, categoryName } = props;
+
+  const queryParams = useSearchParams();
+  const pid = queryParams.get("pid");
+  const [productApiRes, setProductApiRes] = useState<any[]>(product.data);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [categoryNameApiRes, setCategoryNameApiRes] = useState(categoryName);
+  const theme: any = useSelector<any>((state) => state.themeToggle);
+  const isMobile = useMobile();
+  const isTablet = useTablet();
+
+  // useEffect(() => {
+  //   axios({
+  //     method: "GET",
+  //     url: `/api/category/?cid=${params.slug}`,
+  //   })
+  //     .then((res) => {
+  //       if (res.status === 200) {
+  //         if (!res.data.data[0]._id) {
+  //           return;
+  //         }
+  //         axios({
+  //           method: "GET",
+  //           url: `/api/product/?category=${res.data.data[0]._id}`,
+  //         })
+  //           .then((res) => {
+  //             if (res.status === 200) {
+  //               setIsLoading(false);
+  //               setProductApiRes(res.data.data);
+  //             }
+  //           })
+  //           .catch((err) => {
+  //             console.log(err);
+  //             setIsLoading(false);
+  //           });
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, [params.slug]);
+
+  return (
+    <>
+      <PageSpacing>
+        {isMobile ? (
+          ""
+        ) : (
+          <Box paddingLeft="2rem" marginTop="2rem">
+            <BreadCrumb />
+          </Box>
+        )}
+
+        <Box paddingLeft="2rem" margin={"2rem 0"}>
+          <Typography
+            color={
+              theme === "light"
+                ? lightColor.text.primary
+                : darkColor.text.primary
+            }
+            fontSize={isMobile ? "1.8rem" : "2.8rem"}
+            fontStyle="normal"
+            fontWeight="700"
+            lineHeight="normal"
+            letterSpacing="0.02rem"
+          >
+            {categoryNameApiRes}
+          </Typography>
+        </Box>
+
+        {/* Filter feature UI */}
+        {/* <Box textAlign="end" gap="0.5rem">
+          <Typography
+            color={color.text.primary}
+            fontSize="2rem"
+            fontStyle="normal"
+            fontWeight="700"
+            lineHeight="normal"
+            letterSpacing="0.02rem"
+          >
+            Filter
+          </Typography>
+        </Box> */}
+
+        {isMobile || isTablet ? (
+          // is mobile and tablet size is active
+          <Grid
+            container
+            justifyContent="center"
+            padding="0 2rem"
+            spacing={0.5}
+          >
+            {isLoading ? (
+              [...Array(4)].map((data, index) => {
+                return (
+                  <Grid
+                    key={`${index}+ProductCardOnSaleSkeletonMobile`}
+                    item
+                    xs={6}
+                    sm={4}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <ProductCardSkeleton />
+                  </Grid>
+                );
+              })
+            ) : productApiRes.length === 0 ? (
+              <Box width="100%" textAlign="center">
+                <NoProduct isMobile={isMobile} />
+              </Box>
+            ) : (
+              productApiRes.map((data, index) => {
+                return (
+                  <Grid
+                    item
+                    xs={6}
+                    sm={4}
+                    key={data._id}
+                    margin="1rem 0"
+                    justifyItems="center"
+                  >
+                    <Card data={data} index={index} />
+                  </Grid>
+                );
+              })
+            )}
+          </Grid>
+        ) : (
+          // is desktop size is active
+          <Grid container spacing={2}>
+            {isLoading ? (
+              [...Array(8)].map((data, index) => {
+                return (
+                  <Grid
+                    key={`${index}+ProductCardOnSaleSkeleton`}
+                    item
+                    xs={3}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
+                    <ProductCardSkeleton />
+                  </Grid>
+                );
+              })
+            ) : productApiRes.length === 0 ? (
+              <Box width="100%" textAlign="center">
+                <NoProduct />
+              </Box>
+            ) : (
+              productApiRes.slice(0, 8).map((data, index) => {
+                return (
+                  <>
+                    <Grid
+                      key={data._id}
+                      item
+                      xs={3}
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Card data={data} isHomePage={true} />
+                    </Grid>
+                  </>
+                );
+              })
+            )}
+          </Grid>
+        )}
+      </PageSpacing>
+    </>
+  );
+};
+
+export default ProductCollection;
--- src/app/combos/page.tsx
+++ src/app/combos/page.tsx
@@ -1,106 +1,22 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { Box, Breadcrumbs, Typography, Link, Stack, Grid } from "@mui/material";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import PageSpacing from "@components/PageSpacing";
-import { lightColor, darkColor } from "@/utils/CustomTheme/color";
-import Card from "@components/Card";
-import axios from "axios";
-import BreadCrumb from "@components/BreadCrumb";
-import NoProduct from "../../../icons/noProduct";
-import ProductCardSkeleton from "@components/Skeleton/ProductCardSkeleton";
-import { useMobile } from "@/utils/responsive";
-import { useSelector } from "react-redux";
+import React from "react";
+import Mainpage from "./Mainpage";
 
-const Combos = ({ params }: { params: { slug: string } }) => {
-  const [comboProductApiRes, setComboProductApiRes] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const theme: any = useSelector<any>((state) => state.themeToggle);
-  const isMobile = useMobile();
-
-  useEffect(() => {
-    axios({
-      method: "GET",
-      url: "/api/product/?tag=combo",
-    })
-      .then((res) => {
-        if (res.status === 200) {
-          setIsLoading(false);
-          setComboProductApiRes(res.data.data);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  }, []);
-
-  return (
-    <>
-      <PageSpacing>
-        <Box paddingLeft="2rem" marginTop="2rem">
-          <BreadCrumb />
-        </Box>
-
-        <Box paddingLeft="2rem" margin="1rem 0">
-          <Typography
-            color={theme === "light"
-            ? lightColor.text.primary
-            : darkColor.text.primary}
-            fontSize="2.8rem"
-            fontStyle="normal"
-            fontWeight="700"
-            lineHeight="normal"
-            letterSpacing="0.02rem"
-          >
-            Combos
-          </Typography>
-        </Box>
-        {/* <Box textAlign="end" gap="0.5rem">
-          <Typography
-            color={theme === "light"
-            ? lightColor.text.primary
-            : darkColor.text.primary}
-            fontSize="2rem"
-            fontStyle="normal"
-            fontWeight="700"
-            lineHeight="normal"
-            letterSpacing="0.02rem"
-          >
-            Filter
-          </Typography>
-        </Box> */}
-
-        <Grid container spacing={1}>
-          {isLoading ? (
-            [...Array(4)].map((data, index: number) => {
-              return (
-                <Grid
-                  key={`${index}+ProductCardNewArrivalsSkeleton`}
-                  item
-                  xs={3}
-                >
-                  <ProductCardSkeleton />
-                </Grid>
-              );
-            })
-          ) : comboProductApiRes.length === 0 ? (
-            <Box width="100%" textAlign="center">
-              <NoProduct isMobile={isMobile} />
-            </Box>
-          ) : (
-            comboProductApiRes.map((data) => {
-              return (
-                <Grid key={data._id} item xs={3}>
-                  <Card data={data} />
-                </Grid>
-              );
-            })
-          )}
-        </Grid>
-      </PageSpacing>
-    </>
-  );
+export const metadata = {
+  title: "Combo",
+  openGraph: {
+    title: "Combo",
+  },
 };
 
-export default Combos;
+export const dynamic = "force-dynamic"
+
+const Page = async ({ params }: { params: { slug: string } }) => {
+
+    let productRes = await fetch(`http://localhost:3000/api/product/?category=${"66afb1e011d163c99f8613b9"}`);
+    let product = await productRes.json()
+  return (
+    <Mainpage params={params} product={product}/>
+    // <></>
+  );
+};
+export default Page;
