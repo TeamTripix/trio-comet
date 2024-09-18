5f86676813a887f32062c6ae2020dbfb58c99c00
Himanshu <himanshuflexsin@gmail.com>
2024-09-18 12:44:05 +0530

    add docker container

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
       {
         name: "trio-comet",
-        script: "npm start",
+        script: "docker run -dp 3000:3000 triocomet",
         port: 3000
       }
   ]
